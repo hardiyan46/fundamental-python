72697780a238eb54f36a9c39ac9412b7bca26511
hardiyan.purnama.hp@gmail.com <smart_m07>
2020-07-23 15:32:01 +0700

    Perulangan dengan menampilkan nama anak dari 1 ke 10

---
+++ perulangan.py
@@ -0,0 +1,8 @@
+jumlah_anak = 10
+
+for index_anak in range (1, jumlah_anak+1):  #python menghitung mulai dari 0
+    print(f'Hallo anak ke {index_anak}')
+
+"""
+perintah di atas untuk mengulang pemanggilan anak dari 1 ke 10
+"""
