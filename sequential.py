971aba5f1f55f6ce097bc3958ea4222a83f67715
hardiyan.purnama.hp@gmail.com <smart_m07>
2020-07-23 14:37:06 +0700

    penambahan python file percabangan.py

--- sequential.py
+++ sequential.py
@@ -1,13 +1,13 @@
 """
 sequential code di running dengan berurutan
-jika kode diatasnya salah maka tidak akan dilanjutkan ke code selanjutnya
+jika kode diatasnya salah/error maka tidak akan dilanjutkan ke code selanjutnya
 """
 import datetime
 now = datetime.datetime.now()
-print('#'*20)
+print('#'*30)
 print('Belajar Python')
 print(f'pada tanggal {now.strftime("%d-%m-%Y %H:%M:%S")}')
-print('#'*20)
+print('#'*30)
 
 #command diatas berfungi menunjukan waktu dengan import package datetime
 #dan mengatur format date time dengan strftime
