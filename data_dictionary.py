07bfbdc282330443c17b34f36b8bdc73efcb2e57
Hardiyan Purnama <hardiyan.purnama.hp@gmail.com>
2020-07-25 22:58:34 +0700

    Perubahan Tanggal menggunakan datetime

--- data_dictionary.py
+++ data_dictionary.py
@@ -3,6 +3,9 @@ Tipe data Dictionary menghubungkan antara KEY dan VALUE
 KVP = KEY VALUE PAIR
 Dictionary = Kamus
 """
+import datetime
+now = datetime.datetime.now()
+
 #CONTOH 1
 kamus = {}
 kamus  ['anak'] = 'son'
@@ -14,7 +17,7 @@ print(kamus['ibu'])
 #CONTOH 2
 print('\nIni CONTOH ke 2')
 data_dari_server_gojek = {
-    'tanggal': '2020-07-25',
+    'tanggal': {now.strftime("%d-%m-%Y %H:%M:%S")},
     'driver_list': [                        #Penggunaan data List dengan Data Dictionary
         {'nama': 'Hardiyan', 'jarak': 10},
         {'nama': 'Devi', 'jarak': 20},
@@ -28,4 +31,4 @@ print(f"Driver disekitar Anda {data_dari_server_gojek['driver_list']}")
 print(f"Driver #1{data_dari_server_gojek['driver_list'][0]}")
 print(f"Driver  #2{data_dari_server_gojek['driver_list'][1]}")
 print(f"Driver #3{data_dari_server_gojek['driver_list'][2]}")
-print(f"Jarak Driver terdekat adalah {data_dari_server_gojek['driver_list'][0]['jarak']} meter")
+print(f"Sekarang tanggal {data_dari_server_gojek['tanggal']} Jarak Driver terdekat adalah {data_dari_server_gojek['driver_list'][0]['jarak']} meter")
