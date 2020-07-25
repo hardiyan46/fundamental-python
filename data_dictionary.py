4d54fdd2aa1afafd038445043bd60758ff3c15e9
hardiyan.purnama.hp@gmail.com <smart_m07>
2020-07-25 23:30:54 +0700

    penambahan keterangan

--- data_dictionary.py
+++ data_dictionary.py
@@ -7,12 +7,12 @@ import datetime
 now = datetime.datetime.now()
 
 #CONTOH 1
-kamus = {}
+kamus = {}                  Contoh data dict simple
 kamus  ['anak'] = 'son'
 kamus  ['istri'] = 'wife'
 kamus  ['ayah'] = 'father'
 kamus  ['ibu'] = 'mother'
-print(kamus['ibu'])
+print(kamus['ibu'])   #Ambil Value"ibu" didalam data Dict
 
 #CONTOH 2
 print('\nIni CONTOH ke 2')
@@ -29,6 +29,6 @@ data_dari_server_gojek = {
 print(data_dari_server_gojek)
 print(f"Driver disekitar Anda {data_dari_server_gojek['driver_list']}")
 print(f"Driver #1{data_dari_server_gojek['driver_list'][0]}")
-print(f"Driver  #2{data_dari_server_gojek['driver_list'][1]}")
+print(f"Driver #2{data_dari_server_gojek['driver_list'][1]}")
 print(f"Driver #3{data_dari_server_gojek['driver_list'][2]}")
 print(f"Sekarang tanggal {data_dari_server_gojek['tanggal']} Jarak Driver terdekat adalah {data_dari_server_gojek['driver_list'][0]['jarak']} meter")
